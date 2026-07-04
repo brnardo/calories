16bdc5b2c5e1977b9368686228b0605a114284fa
ber <bernardoreynolds@gmail.com>
2026-07-04 13:17:51 +0100

    Center the add FAB plus icon with an SVG instead of a text glyph.
    The text "+" sat off-center on iOS due to font metrics; an SVG cross aligns properly in the circular button.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump the version on every deploy so clients pick up new assets.
-const CACHE = 'calories-v15';
+const CACHE = 'calories-v16';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
