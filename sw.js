1637681f6da56bb0df4bf64aad7df7ed339077aa
ber <bernardoreynolds@gmail.com>
2026-07-04 13:57:52 +0100

    Move delete to sheet header, colored macro dots, and new ingredients.
    Delete sits top-right when editing meals; P/C/F/Fb labels become colored dots everywhere; add azeite, queque de chocolate, and olive oil from label data.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump the version on every deploy so clients pick up new assets.
-const CACHE = 'calories-v18';
+const CACHE = 'calories-v22';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
